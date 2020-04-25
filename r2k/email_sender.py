0236d669a24b6012cf74df5d53dc3896b558d7d0
Pavel Brodsky <pavel.brodsky@forter.com>
2020-04-25 19:23:46 +0300

    Refactor the email sender a bit to support HTML and lists of URLs

--- r2k/email_sender.py
+++ r2k/email_sender.py
@@ -1,6 +1,6 @@
 import smtplib
 from email.message import EmailMessage
-from typing import Optional
+from typing import List, Optional, Tuple
 
 from r2k.cli import logger
 
@@ -16,25 +16,37 @@ def build_basic_message(title: str) -> EmailMessage:
     return msg
 
 
-def send_email_message(msg: EmailMessage) -> bool:
+def send_email_message(server: smtplib.SMTP, msg: EmailMessage) -> bool:
+    """Send a single EmailMessage"""
+    logger.debug("Sending the email...")
+    try:
+        server.send_message(msg)
+        return True
+    except smtplib.SMTPException as e:
+        logger.error(f"Caught an exception while trying to send an email.\nError: {e}")
+        return False
+
+
+def send_email_messages(msgs: List[EmailMessage]) -> int:
     """Send an email"""
     # TODO: Consider making smtp server configurable
+    messages_sent = 0
     try:
         logger.debug("Connecting to SMTP...")
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
             server.ehlo()
             logger.debug("Logging into the SMTP server...")
             server.login(config.send_from, config.password)
-            logger.debug("Sending the email...")
-            server.send_message(msg)
-            logger.info("Email sent successfully!")
+            for msg in msgs:
+                if send_email_message(server, msg):
+                    messages_sent += 1
+                    logger.info("Email sent successfully!")
     except smtplib.SMTPException as e:
         logger.error(f"Caught an exception while trying to send an email.\nError: {e}")
-        return False
-    return True
+    return messages_sent
 
 
-def set_content(msg: EmailMessage, title: str, url: str, content: Optional[str]) -> None:
+def set_content(msg: EmailMessage, title: str, url: Optional[str], content: Optional[str]) -> None:
     """Either set the text content of the email message, or attach an attachment, based on the current parser"""
     if content:
         filename = f"{title}.html"
@@ -45,14 +57,27 @@ def set_content(msg: EmailMessage, title: str, url: str, content: Optional[str])
             subtype=f'html; charset=utf-8; name="{filename}"',
             filename=filename,
         )
-    else:
+    elif url:
         logger.debug(f"Setting email content to {url}")
         msg.set_content(url)
 
 
-def send_webpage_to_kindle(title: str, url: str, content: Optional[str]) -> bool:
-    """Send a webpage to Kindle"""
+def create_email_message(title: str, url: Optional[str], content: Optional[str]) -> EmailMessage:
     title = strip_common_unicode_chars(title)
     msg = build_basic_message(title)
     set_content(msg, title, url, content)
-    return send_email_message(msg)
+    return msg
+
+
+def send_html(title: str, content: str) -> int:
+    """Send an HTML file to Kindle"""
+    msg = create_email_message(title, None, content)
+    return send_email_messages([msg])
+
+
+def send_urls(articles: List[Tuple[str, str]]) -> int:
+    """Send a list of URLs to Kindle (via pushtokindle)"""
+    msgs = []
+    for title, url in articles:
+        msgs.append(create_email_message(title, url, None))
+    return send_email_messages(msgs)
