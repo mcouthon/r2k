f3b7ed3d8d718f604b6fbf770363795a8d2d7b39
Pavel Brodsky <pavel.brodsky@forter.com>
2020-04-25 12:47:56 +0300

    Add a --json option to showing the config, to use with e.g. 'jq'

--- r2k/cli/config/config_show.py
+++ r2k/cli/config/config_show.py
@@ -1,4 +1,5 @@
 import click
+import orjson as json
 import yaml
 
 from r2k.cli import cli_utils, logger
@@ -7,9 +8,20 @@ from r2k.config import config as _config
 
 @click.command("show")
 @cli_utils.config_path_option()
-def config_show() -> None:
+@click.option(
+    "-j",
+    "--json",
+    "is_json",
+    is_flag=True,
+    help="When passed the output will be in JSON format (e.g. for use with jq).\n"
+    "Use with the --no-ansi flag for best results",
+)
+def config_show(is_json: bool) -> None:
     """Show all the available configuration."""
     result = _config.as_dict()
     if "password" in result:
         result["password"] = "XXXXXXX"
-    logger.info(yaml.safe_dump(result))
+    if is_json:
+        logger.info(json.dumps(result))
+    else:
+        logger.info(yaml.safe_dump(result))
