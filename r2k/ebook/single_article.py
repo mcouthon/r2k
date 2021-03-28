97163e5c63aa3a298519575fe4788df42ccc57ae
Pavel Brodsky <pavel.brodsky@forter.com>
2021-03-28 14:53:03 +0300

    Attempt to set author, as well as title

--- r2k/ebook/single_article.py
+++ r2k/ebook/single_article.py
@@ -10,14 +10,23 @@ class SingleArticle(Article):
     def __init__(self, url: str) -> None:
         """Constructor"""
         super().__init__({"link": url})
-
-        self.title = self.get_title()
-
-    def get_title(self) -> str:
-        """Find the article's title"""
         reqs = requests.get(self.link)
-        soup = BeautifulSoup(reqs.text, "html.parser")
+        self._soup = BeautifulSoup(reqs.text, "html.parser")
 
-        for title in soup.find_all("title"):
-            return title.get_text()
-        raise RuntimeError("No title found for article")
+        self.set_title()
+        self.set_author()
+
+    def set_title(self) -> None:
+        """Set the article's title"""
+        titles = self._soup.find_all("title")
+        if titles:
+            self.title = titles[0].get_text()
+        else:
+            self.title = self.link
+
+    def set_author(self) -> None:
+        """Set the article's author"""
+        metas = self._soup.find_all("meta")
+        for meta in metas:
+            if meta.attrs.get("name") == "author":
+                self.author = meta.attrs.get("content")
